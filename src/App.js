4158780eca56257bebce9b39e19d89e051369047
H4nsCast0rp <zakmysteryo@gmail.com>
2022-05-25 10:13:28 +0000

    v0.001 liste

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: "flex", justifyContent: "center" }}>
+      <Characters />
     </div>
   );
+}
+
+function Characters() {
+  const backendData = data;
+  console.log(backendData);
+
+  const charsRootStyle = {
+    border: "2px #0af solid",
+    borderRadius: 9,
+    margin: 20,
+    backgroundColor: "#efefef",
+    padding: 6
+  };
+
+  return (
+    <div style={{ width: 400 }}>
+      {backendData.map(ele => 
+        <div style={charsRootStyle}>
+          <h3>{ele.characters}</h3>
+          <p>{ele.traduction}</p>
+          <small>{ele.pinyin}</small>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default App;
